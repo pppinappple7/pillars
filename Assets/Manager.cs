9d8d7f47ca58811108f10cc35d7b74a2f31ab48c
markcool2010 <markcool201039@gmail.com>
2026-09-21 21:15:38 +0200

    менюшки и спавн через пул

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -3,23 +3,44 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    [SerializeField]GameObject pillar;
+    
+    [SerializeField] GameObject pause;
+    [SerializeField] float spawnTime;
+    [SerializeField]float spawnTimer;
+    [SerializeField]GameObject[] pillarPull;
     public List<Vector3> usedPos;
     public List<char> usedChars;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Time.timeScale = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            pause.SetActive(true);
+        }
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer > spawnTime)
+        {
+            SpawnPillar();
+            spawnTimer = 0;
+        }
     }
     void SpawnPillar()
     {
-        Instantiate(pillar);
+        foreach(GameObject pillarObj in pillarPull)
+        {
+            if(!pillarObj.activeSelf)
+            {
+                pillarObj.SetActive(true);
+                return;
+                
+            }
+        }
     }
 }
--- Assets/pillar.cs
+++ Assets/pillar.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class pillar : MonoBehaviour
 {
+    [SerializeField]Mesh initialMesh;
+    MeshFilter meshFilter;
+    [SerializeField] float breakWarnTime;
+    [SerializeField] Mesh broken;
+    [SerializeField] float breakTime;
+    float breakTimer;
     [SerializeField] TextMeshProUGUI letterText;
     GameObject player;
     Manager manager;
@@ -13,19 +19,43 @@ public class pillar : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        manager=GameObject.Find("manager").GetComponent<Manager>();
-        charecter = UnusedChar();
-        manager.usedChars.Add(UnusedChar());
-        transform.position = RandomStartPos();
-        player = GameObject.Find("player");
-    }
+        
 
+    }
+    private void OnEnable()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        player = GameObject.Find("player");
+        manager = GameObject.Find("manager").GetComponent<Manager>();
+        manager.usedChars.Add(charecter);
+   
+        charecter = UnusedChar();
+        
+        meshFilter.mesh=initialMesh;
+        transform.position = RandomStartPos();
+        breakTimer = 0;
+    }
     // Update is called once per frame
     void Update()
     {
+        breakTimer += Time.deltaTime;
+        if(breakTimer>=breakTime-breakWarnTime)
+        {
+            if(meshFilter.mesh!=broken)
+            {
+                meshFilter.mesh = broken;
+            }
+        }
+        if(breakTimer>=breakTime)
+        {
+            manager.usedPos.Remove(new Vector3(transform.position.x, -24.95f, transform.position.z));
+            manager.usedChars.Remove(charecter);
+            gameObject.SetActive(false);
+            
+        }
         if(transform.position.y< -11.5)
         {
-            transform.Translate(Vector3.up *5* Time.deltaTime);
+            transform.Translate(Vector3.up *15* Time.deltaTime);
             
         }
         else if(transform.position.y> -11.5)
@@ -46,14 +76,14 @@ public class pillar : MonoBehaviour
     {
         float x = Random.Range(-2, 3) * 4;
         float z = Random.Range(0,3) * 2;
-        if(manager.usedPos.Contains(new Vector3(x, -15.7f, z)))
+        if(manager.usedPos.Contains(new Vector3(x, -24.95f, z)))
         {
             return RandomStartPos();
         }
         else
         {
-            manager.usedPos.Add(new Vector3(x, -15.7f, z));
-            return new Vector3(x, -15.7f, z);
+            manager.usedPos.Add(new Vector3(x, -24.95f, z));
+            return new Vector3(x, -24.95f, z);
         }
             
     }
@@ -66,6 +96,7 @@ public class pillar : MonoBehaviour
         }
         else
         {
+            
             keyCode=(KeyCode)System.Enum.Parse(typeof(KeyCode),ch.ToString());
             letterText.text = ch.ToString();
             return ch;
